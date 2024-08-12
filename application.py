0dcbea1cf07dec6a3de689f3bd9578f297893013
Aryan Kelwa <aryankelwa7024@gmail.com>
2024-08-12 12:36:28 +0530

    adding new files for deplyment

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
-app=Flask(__name__)
+application=Flask(__name__)
+app=application
 
 ridge_modle=pickle.load(open(r'models\algerian_ridge_model.pkl','rb'))
 scaler=pickle.load(open(r'models\algerian_ridge_scaler.pkl','rb'))
